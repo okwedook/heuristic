054f4207cf0f3d0eac9df46fc5fd77d1b529251d
Mikhail Pogodin <m.pogodin@vk.team>
2025-01-02 01:51:18 +0300

    Add CustomBagOfCells class declaration, which copies BagOfCells from TON

--- ton-local-tester/solution.cpp
+++ ton-local-tester/solution.cpp
@@ -12,6 +12,148 @@
 #include "vm/boc.h"
 #include "block/block-auto.h"
 
+namespace vm {
+class CustomBagOfCells {
+ public:
+  enum { hash_bytes = vm::Cell::hash_bytes, default_max_roots = 16384 };
+  enum Mode { WithIndex = 1, WithCRC32C = 2, WithTopHash = 4, WithIntHashes = 8, WithCacheBits = 16, max = 31 };
+  enum { max_cell_whs = 64 };
+  using Hash = Cell::Hash;
+  struct Info {
+    enum : td::uint32 { boc_idx = 0x68ff65f3, boc_idx_crc32c = 0xacc3a728, boc_generic = 0xb5ee9c72 };
+
+    unsigned magic;
+    int root_count;
+    int cell_count;
+    int absent_count;
+    int ref_byte_size;
+    int offset_byte_size;
+    bool valid;
+    bool has_index;
+    bool has_roots{false};
+    bool has_crc32c;
+    bool has_cache_bits;
+    unsigned long long roots_offset, index_offset, data_offset, data_size, total_size;
+    Info() : magic(0), valid(false) {
+    }
+    void invalidate() {
+      valid = false;
+    }
+    long long parse_serialized_header(const td::Slice& slice);
+    unsigned long long read_int(const unsigned char* ptr, unsigned bytes);
+    unsigned long long read_ref(const unsigned char* ptr) {
+      return read_int(ptr, ref_byte_size);
+    }
+    unsigned long long read_offset(const unsigned char* ptr) {
+      return read_int(ptr, offset_byte_size);
+    }
+    void write_int(unsigned char* ptr, unsigned long long value, int bytes);
+    void write_ref(unsigned char* ptr, unsigned long long value) {
+      write_int(ptr, value, ref_byte_size);
+    }
+    void write_offset(unsigned char* ptr, unsigned long long value) {
+      write_int(ptr, value, offset_byte_size);
+    }
+  };
+
+ private:
+  int cell_count{0}, root_count{0}, dangle_count{0}, int_refs{0};
+  int int_hashes{0}, top_hashes{0};
+  int max_depth{1024};
+  Info info;
+  unsigned long long data_bytes{0};
+  td::HashMap<Hash, int> cells;
+  struct CellInfo {
+    Ref<DataCell> dc_ref;
+    std::array<int, 4> ref_idx;
+    unsigned char ref_num;
+    unsigned char wt;
+    unsigned char hcnt;
+    int new_idx;
+    bool should_cache{false};
+    bool is_root_cell{false};
+    CellInfo() : ref_num(0) {
+    }
+    CellInfo(Ref<DataCell> _dc) : dc_ref(std::move(_dc)), ref_num(0) {
+    }
+    CellInfo(Ref<DataCell> _dc, int _refs, const std::array<int, 4>& _ref_list)
+        : dc_ref(std::move(_dc)), ref_idx(_ref_list), ref_num(static_cast<unsigned char>(_refs)) {
+    }
+    bool is_special() const {
+      return !wt;
+    }
+  };
+  std::vector<CellInfo> cell_list_;
+  struct RootInfo {
+    RootInfo() = default;
+    RootInfo(Ref<Cell> cell, int idx) : cell(std::move(cell)), idx(idx) {
+    }
+    Ref<Cell> cell;
+    int idx{-1};
+  };
+  std::vector<CellInfo> cell_list_tmp;
+  std::vector<RootInfo> roots;
+  std::vector<unsigned char> serialized;
+  const unsigned char* index_ptr{nullptr};
+  const unsigned char* data_ptr{nullptr};
+  std::vector<unsigned long long> custom_index;
+
+ public:
+  void clear();
+  int set_roots(const std::vector<td::Ref<vm::Cell>>& new_roots);
+  int set_root(td::Ref<vm::Cell> new_root);
+  int add_roots(const std::vector<td::Ref<vm::Cell>>& add_roots);
+  int add_root(td::Ref<vm::Cell> add_root);
+  td::Status import_cells() TD_WARN_UNUSED_RESULT;
+  CustomBagOfCells() = default;
+  std::size_t estimate_serialized_size(int mode = 0);
+  td::Status serialize(int mode = 0);
+  td::string serialize_to_string(int mode = 0);
+  td::Result<td::BufferSlice> serialize_to_slice(int mode = 0);
+  td::Result<std::size_t> serialize_to(unsigned char* buffer, std::size_t buff_size, int mode = 0);
+  td::Status serialize_to_file(td::FileFd& fd, int mode = 0);
+  template <typename WriterT>
+  td::Result<std::size_t> serialize_to_impl(WriterT& writer, int mode = 0);
+  std::string extract_string() const;
+
+  td::Result<long long> deserialize(const td::Slice& data, int max_roots = default_max_roots);
+  td::Result<long long> deserialize(const unsigned char* buffer, std::size_t buff_size,
+                                    int max_roots = default_max_roots) {
+    return deserialize(td::Slice{buffer, buff_size}, max_roots);
+  }
+  int get_root_count() const {
+    return root_count;
+  }
+  Ref<Cell> get_root_cell(int idx = 0) const {
+    return (idx >= 0 && idx < root_count) ? roots.at(idx).cell : Ref<Cell>{};
+  }
+
+  static int precompute_cell_serialization_size(const unsigned char* cell, std::size_t len, int ref_size,
+                                                int* refs_num_ptr = nullptr);
+
+ private:
+  int rv_idx;
+  td::Result<int> import_cell(td::Ref<vm::Cell> cell, int depth);
+  void cells_clear() {
+    cell_count = 0;
+    int_refs = 0;
+    data_bytes = 0;
+    cells.clear();
+    cell_list_.clear();
+  }
+  td::uint64 compute_sizes(int mode, int& r_size, int& o_size);
+  void reorder_cells();
+  int revisit(int cell_idx, int force = 0);
+  unsigned long long get_idx_entry_raw(int index);
+  unsigned long long get_idx_entry(int index);
+  bool get_cache_entry(int index);
+  td::Result<td::Slice> get_cell_slice(int index, td::Slice data);
+  td::Result<td::Ref<vm::DataCell>> deserialize_cell(int index, td::Slice data, td::Span<td::Ref<DataCell>> cells,
+                                                     std::vector<td::uint8>* cell_should_cache);
+};
+
+} // namespace vm
+
 td::BufferSlice compress(td::Slice data) {
   td::Ref<vm::Cell> root = vm::std_boc_deserialize(data).move_as_ok();
   td::BufferSlice serialized = vm::std_boc_serialize(root, 0).move_as_ok();
